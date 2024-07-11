fc92ad07101c163e36c06d65f042a47cc90396b0
Nurdaulet <nurik93sat@gmail.com>
2024-07-11 05:32:10 +0500

    Multiple slides to show

--- projects/app/src/app/slides-data.ts
+++ projects/app/src/app/slides-data.ts
@@ -5,24 +5,48 @@ export const slides: Slide[] = [
     id: 1,
     image: 'https://images.pexels.com/photos/9551192/pexels-photo-9551192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     url: 'https://www.youtube.com/',
-    priority: 3
+    priority: 1
   },
   {
     id: 2,
     image: 'https://images.pexels.com/photos/26926257/pexels-photo-26926257/free-photo-of-just-chillin.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     url: 'https://www.google.com',
-    priority: 2
+    priority: 1
   },
   {
     id: 3,
     image: 'https://images.pexels.com/photos/23020602/pexels-photo-23020602/free-photo-of-a-fountain-with-statues-in-front-of-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     url: 'https://dribbble.com/',
-    priority: 10
+    priority: 1
   },
   {
-    id: 3,
+    id: 4,
     image: 'https://images.pexels.com/photos/26811653/pexels-photo-26811653/free-photo-of-photo-of-a-man-jumping-a-motorbike-midair.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     url: 'https://angular.dev/',
-    priority: 4
+    priority: 1
+  },
+  {
+    id: 5,
+    image: 'https://images.pexels.com/photos/18133948/pexels-photo-18133948/free-photo-of-portrait-of-woman-in-traditional-clothing-and-hat.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://angular.dev/',
+    priority: 1
+  },
+  {
+    id: 6,
+    image: 'https://images.pexels.com/photos/18595564/pexels-photo-18595564/free-photo-of-vases-with-flowers-on-table.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://angular.dev/',
+    priority: 1
+  },
+  {
+    id: 7,
+    image: 'https://images.pexels.com/photos/3413334/pexels-photo-3413334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://angular.dev/',
+    priority: 1
+  },
+  {
+    id: 8,
+    image: 'https://images.pexels.com/photos/19036833/pexels-photo-19036833/free-photo-of-view-of-autumnal-trees-and-a-rocky-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://angular.dev/',
+    priority: 1
   },
 ]
--- projects/slider/src/lib/slider/slider.component.ts
+++ projects/slider/src/lib/slider/slider.component.ts
@@ -6,24 +6,35 @@ import {
   signal
 } from '@angular/core';
 import { Slide } from './slide.model';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'lib-slider',
   standalone: true,
-  imports: [],
+  imports: [
+    NgStyle
+  ],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SliderComponent implements OnInit {
-  delay = input<number>(3000);
   _slides = input<Slide[]>([], { alias: 'slides'});
   private slides = computed(() => this._slides().sort((a, b) => b.priority -  a.priority));
+
+  delay = input<number>(2000);
+  slidesShow = input(1);
   private currentIndex = signal(0);
-  currentSlide = computed(() => this.slides()[this.currentIndex()]);
+
+  currentSlides = computed(() => {
+    if (this.slidesShow() + this.currentIndex() > this.slides().length) {
+      return [...this.slides().slice(this.currentIndex()), ...this.slides().slice(0, this.slidesShow() - (this.slides().length - this.currentIndex()))];
+    }
+    return this.slides().slice(this.currentIndex(), this.currentIndex() + this.slidesShow());
+  });
 
   ngOnInit(): void {
-    setInterval(() => this.next(), this.delay());
+    // setInterval(() => this.next(), this.delay());
   }
 
   prev() {
@@ -34,5 +45,9 @@ export class SliderComponent implements OnInit {
   next() {
     this.currentIndex.set(this.currentIndex() + 1);
     if (this.currentIndex() >= this.slides().length) this.currentIndex.set(0);
+  }
+
+  getSlideUrl(url: string) {
+    return `url('${url}')`
   }
 }
